038fbac1609656c0b952af724463c22b81602330
Mfundo <mfundosibisi2004gnail.com>
2023-02-16 17:09:25 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include < stdio.h
+#include <stdio.h>
 /**
- * main a programme that prints a line with puts function
- * Return 0 (Success)
- */
+ * main - Entry point;
+ * Return: 0 (Success);
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
