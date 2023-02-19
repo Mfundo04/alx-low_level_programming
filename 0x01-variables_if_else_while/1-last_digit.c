da9ae3bb7775644024644b3731bddf4bcdb7c08a
Mfundo <mfundosibisi2004gnail.com>
2023-02-19 00:50:28 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print statemnets based on the last digit of the random number
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int n;
+	int m;
+
+	srnad(time(0));
+	n rand(void) - RAND_MAX / 2;
+	m = n % 10;
+	else if (m > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, m);
+	else if (m = 0)
+		printf("Last digit of %i is %i and is 0\n", n, m);
+	else if (m < 6)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, m);
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet in reverse order in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ci;
+	for (c = 'z'; c >= 'a'; c--)
+		
+	{
+		putchar(c);
+	}
+		putchar('\n');
+		return (0);
+}
+
