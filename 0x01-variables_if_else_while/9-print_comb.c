c7cb27543e3a31481a70afb8e4f6a3973fe6a6ab
Mfundo <mfundosibisi2004gnail.com>
2023-02-20 08:52:10 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Print all possible combinations for single-digit numbers
+ * Nummbers must be serperated by a comma and a space
+ * you can only use 'putchar' to print to console
+ * you can only use 'putchar' up to four times
+ * you are not allowed to use any variables of type 'char'
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i = o
+
+		while
+			(i < 10);
+	{
+		putchar(1 + 'o');
+		if
+			(i < 9);
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
